83c45523b4264874fc946393578003aa44926a92
Owais <owais.quadri@ontariotechu.net>
2021-04-30 14:36:41 -0400

    Basic HTML/CSS/Electron/JavaScript/Bootstrap configurations

--- main/main.js
+++ main/main.js
@@ -2,19 +2,54 @@
 const { app , BrowserWindow , Menu , ipcMain } = require('electron')
 const path = require('path')
 const url = require('url')
+//set env
+process.env.NODE_ENV="development"
 //create window
 let mainWindow;
 function createWindow(){
     //configure window
     mainWindow = new BrowserWindow({
         width:800,
-        height:600
+        height:600,
+        minWidth:400,
+        minHeight:400
     });
     //configure main menu
-    
-
+    let mainMenu=[
+        {
+            label:'File',
+            submenu:[{
+                label:'Some Action'
+            },{type:'separator'},{
+                label:'Quit',
+                accelerator:'CmdOrCtrl+Q',
+                click(){
+                    app.quit()
+                }
+            }]
+        }
+        
+    ]
+    //if mac, shift start
+    if(process.platform=="darwin"){mainMenu.unshift({})}
+    //if development, add devtools
+    if(process.env.NODE_ENV!="production"){mainMenu.push({
+        label:'DevTools',
+        submenu:[{
+            label:'Toggle DevTools',
+            role:"toggleDevTools",
+            accelerator: 'CmdOrCtrl+I',
+            acceleratorWorksWhenHidden:false,
+        },{
+            label:'Reload',
+            role:'reload',
+            accelerator:'F5'
+        }]
+    })}
+    //load menu
+    const appMenu=Menu.buildFromTemplate(mainMenu)
+    Menu.setApplicationMenu(appMenu)
     //load window
-    mainWindow.webContents.openDevTools()
     mainWindow.loadURL(path.join("file://",__dirname,"../renderer/index.html"))
     mainWindow.show()
 }
