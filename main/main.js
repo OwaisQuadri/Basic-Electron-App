895ba82ffb6d20a22b433e57049b7f0c29f48118
Owais <owais.quadri@ontariotechu.net>
2021-05-01 14:31:47 -0400

    front-end config

--- main/main.js
+++ main/main.js
@@ -1,61 +1,66 @@
 //imports
-const { app , BrowserWindow , Menu , ipcMain } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
 const path = require('path')
 const url = require('url')
 //set env
-process.env.NODE_ENV="development"
+process.env.NODE_ENV = "development"
 //create window
 let mainWindow;
-function createWindow(){
+function createWindow() {
     //configure window
     mainWindow = new BrowserWindow({
-        width:800,
-        height:600,
-        minWidth:400,
-        minHeight:400
+        width: 800,
+        height: 600,
+        minWidth: 400,
+        minHeight: 400,
+        webPreferences: {
+            nodeIntegration: true
+        }
     });
     //configure main menu
-    let mainMenu=[
+    let mainMenu = [
         {
-            label:'File',
-            submenu:[{
-                label:'Some Action'
-            },{type:'separator'},{
-                label:'Quit',
-                accelerator:'CmdOrCtrl+Q',
-                click(){
+            label: 'File',
+            submenu: [{
+                label: 'Some Action'
+            }, { type: 'separator' }, {
+                label: 'Quit',
+                accelerator: 'CmdOrCtrl+Q',
+                click() {
                     app.quit()
                 }
             }]
         }
-        
+
     ]
     //if mac, shift start
-    if(process.platform=="darwin"){mainMenu.unshift({})}
+    if (process.platform == "darwin") { mainMenu.unshift({}) }
     //if development, add devtools
-    if(process.env.NODE_ENV!="production"){mainMenu.push({
-        label:'DevTools',
-        submenu:[{
-            label:'Toggle DevTools',
-            role:"toggleDevTools",
-            accelerator: 'CmdOrCtrl+I',
-            acceleratorWorksWhenHidden:false,
-        },{
-            label:'Reload',
-            role:'reload',
-            accelerator:'F5'
-        }]
-    })}
+    if (process.env.NODE_ENV != "production") {
+        mainMenu.push({
+            label: 'DevTools',
+            submenu: [{
+                label: 'Toggle DevTools',
+                role: "toggleDevTools",
+                accelerator: 'CmdOrCtrl+I',
+                acceleratorWorksWhenHidden: false,
+            }, {
+                label: 'Reload',
+                role: 'reload',
+                accelerator: 'F5'
+            }]
+        })
+    }
     //load menu
-    const appMenu=Menu.buildFromTemplate(mainMenu)
+    const appMenu = Menu.buildFromTemplate(mainMenu)
     Menu.setApplicationMenu(appMenu)
     //load window
-    mainWindow.loadURL(path.join("file://",__dirname,"../renderer/index.html"))
+    mainWindow.loadURL(path.join("file://", __dirname, "../renderer/index.html"))
     mainWindow.show()
 }
-app.on('ready', ()=>{
+app.on('ready', () => {
     createWindow()
-    mainWindow.on('closed', ()=>{
+    mainWindow.on('closed', () => {
         app.quit()
     })
 })
